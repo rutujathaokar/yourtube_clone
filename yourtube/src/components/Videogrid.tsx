2eb2701cc430b6db1d0db14d5094b6f97ff0daa0
Rutuja Thaokar <thaokarrutuja18@gmail.com>
2026-06-10 02:53:20 +0530

    Base YouTube clone completed

--- yourtube/src/components/Videogrid.tsx
+++ yourtube/src/components/Videogrid.tsx
@@ -3,7 +3,7 @@ import Videocard from "./videocard";
 import axiosInstance from "@/lib/axiosinstance";
 
 const Videogrid = () => {
-  const [videos, setvideo] = useState<any>(null);
+  const [videos, setvideo] = useState<any>([]);
   const [loading, setloading] = useState(true);
   useEffect(() => {
     const fetchvideo = async () => {
@@ -49,11 +49,25 @@ const Videogrid = () => {
   // ];
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {loading ? (
+      {/* {loading ? (
         <>Loading..</>
       ) : (
-        videos.map((video: any) => <Videocard key={video._id} video={video} />)
-      )}
+        videos?.map((video: any) => ( <Videocard key={video._id} video={video} />)))
+        }   */}
+
+
+
+        {loading ? (
+  <>Loading..</>
+) : videos.length > 0 ? (
+  videos.map((video: any) => (
+    <Videocard key={video._id} video={video} />
+  ))
+) : (
+  <p>No videos found</p>
+)}
+
+
     </div>
   );
 };
--- yourtube/src/components/videocard.tsx
+++ yourtube/src/components/videocard.tsx
@@ -9,10 +9,17 @@ export default function VideoCard({ video }: any) {
     <Link href={`/watch/${video?._id}`} className="group">
       <div className="space-y-3">
         <div className="relative aspect-video rounded-lg overflow-hidden bg-gray-100">
-          <video
+          {/* <video
             src={`${process.env.BACKEND_URL}/${video?.filepath}`}
             className="object-cover group-hover:scale-105 transition-transform duration-200"
-          />
+          /> */}
+
+          <video
+  src={`http://localhost:5000/${video?.filepath.replace(/\\/g, "/")}`}
+  className="object-cover group-hover:scale-105 transition-transform duration-200"
+/>
+
+
           <div className="absolute bottom-2 right-2 bg-black/80 text-white text-xs px-1 rounded">
             10:24
           </div>
